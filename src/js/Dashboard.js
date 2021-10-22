04dd4f5d41db8c957717562d6ed316dd3c95e678
alec-sena <alexanderwsena@gmail.com>
2021-10-22 14:30:33 -0400

    add dashboard logic (WIP)

--- src/js/Dashboard.js
+++ src/js/Dashboard.js
@@ -2,20 +2,30 @@ import PlayerPanel from "./PlayerPanel";
 
 function Dashboard(props) {
 
-    if(numPlayers === 1){
-        return(
-            <div className="Dashboard">
-                <div>Time</div>
-                <div>Moves</div>
-            </div>
-        )
-    } else {
-        return(
-            <div className="Dashboard">
-            </div>
-        )
-    }
+    let JSX;
 
+    if(props.numPlayers < 2){
+        JSX =
+        <>
+            <div>Time</div>
+            <div>Moves</div>
+        </>
+    } else {
+        let panels = [];
+        for(let x = 0; x < props.numPlayers; x++){
+            panels.push(<PlayerPanel />);
+        }
+        JSX = 
+        <>
+            {panels}
+        </>
+    }
+    
+    return(
+            <div className="Dashboard">
+                {JSX}
+            </div>
+        )
 }
 
 export default Dashboard;
